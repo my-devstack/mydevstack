a610f715d85f9cde8e9945e7b6002635ecfaf4cb
Alfonso Rodriguez <beabys@gmail.com>
2026-05-06 10:16:47 +0800

    FIX: solve lint issue

--- pkg/ui/vitest.config.ts
+++ pkg/ui/vitest.config.ts
@@ -4,9 +4,9 @@ import { resolve } from 'path';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -21,35 +21,11 @@ export default defineConfig({
       include: ['src/**/*.ts', 'src/**/*.vue'],
       exclude: ['src/**/*.d.ts', 'src/main.ts', 'src/env.d.ts', 'src/api/client.ts', 'src/vite-env.d.ts']
     },
-    workspace: [{
-      extends: true,
-      test: {
-        globals: true,
-        environment: 'happy-dom',
-        include: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
-        onConsoleLog(log) {
-          if (log.includes('[Vue warn]:')) return false;
-        }
-      }
-    }, {
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: 'playwright',
-          instances: [{
-            browser: 'chromium'
-          }]
-        }
-      }
-    }]
+    globals: true,
+    environment: 'happy-dom',
+    include: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
+    onConsoleLog(log) {
+      if (log.includes('[Vue warn]:')) return false;
+    }
   }
 });
