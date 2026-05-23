5f89422f2ada747337bcca340a66fab722cecd3a
Alfonso Rodriguez <beabys@gmail.com>
2026-05-23 19:35:56 +0800

    REF: clean up stub methods

--- pkg/proxy/internal/adapters/http/msk.go
+++ pkg/proxy/internal/adapters/http/msk.go
@@ -15,30 +15,7 @@ func (h *ProxyHandler) registerMSKRoutes(r chi.Router) {
 		r.Get("/clusters/{clusterArn}/bootstrap-brokers", h.getBootstrapBrokers)
 		r.Get("/clusters/{clusterArn}", h.describeClusterV2)
 		r.Delete("/clusters/{clusterArn}", h.deleteCluster)
-		r.Get("/clusters/{clusterArn}/operations", h.listClusterOperations)
-		r.Get("/clusters/{clusterArn}/operations/{operationArn}", h.describeClusterOperation)
-		r.Get("/clusters/{clusterArn}/nodes", h.listNodes)
-		r.Put("/clusters/{clusterArn}/broker-count", h.updateBrokerCount)
-		r.Put("/clusters/{clusterArn}/broker-storage", h.updateBrokerStorage)
-		r.Put("/clusters/{clusterArn}/broker-type", h.updateBrokerType)
-		r.Put("/clusters/{clusterArn}/connectivity", h.updateConnectivity)
-		r.Put("/clusters/{clusterArn}/monitoring", h.updateMonitoring)
-		r.Put("/clusters/{clusterArn}/security", h.updateSecurity)
-		r.Post("/clusters/{clusterArn}/reboot", h.rebootBroker)
 
-		r.Get("/configurations", h.listConfigurations)
-		r.Get("/configurations/{configArn}", h.describeConfiguration)
-
-		r.Get("/replicators", h.listReplicators)
-		r.Post("/replicators", h.createReplicator)
-		r.Get("/replicators/{replicatorArn}", h.describeReplicator)
-		r.Put("/replicators/{replicatorArn}", h.updateReplicationInfo)
-		r.Delete("/replicators/{replicatorArn}", h.deleteReplicator)
-
-		r.Get("/vpc-connections", h.listVpcConnections)
-		r.Post("/vpc-connections", h.createVpcConnection)
-		r.Get("/vpc-connections/{vpcArn}", h.describeVpcConnection)
-		r.Delete("/vpc-connections/{vpcArn}", h.deleteVpcConnection)
 	})
 }
 
@@ -108,88 +85,4 @@ func (h *ProxyHandler) getBootstrapBrokers(w http.ResponseWriter, r *http.Reques
 	writeJSON(w, http.StatusOK, result)
 }
 
-// Stub functions for MSK routes not yet implemented.
 
-func (h *ProxyHandler) listClusterOperations(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"clusterOperationInfoList": []interface{}{}})
-}
-
-func (h *ProxyHandler) describeClusterOperation(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "describeClusterOperation not yet implemented"})
-}
-
-func (h *ProxyHandler) listNodes(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"nodeInfoList": []interface{}{}})
-}
-
-func (h *ProxyHandler) updateBrokerCount(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "updateBrokerCount not yet implemented"})
-}
-
-func (h *ProxyHandler) updateBrokerStorage(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "updateBrokerStorage not yet implemented"})
-}
-
-func (h *ProxyHandler) updateBrokerType(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "updateBrokerType not yet implemented"})
-}
-
-func (h *ProxyHandler) updateConnectivity(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "updateConnectivity not yet implemented"})
-}
-
-func (h *ProxyHandler) updateMonitoring(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "updateMonitoring not yet implemented"})
-}
-
-func (h *ProxyHandler) updateSecurity(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "updateSecurity not yet implemented"})
-}
-
-func (h *ProxyHandler) rebootBroker(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "rebootBroker not yet implemented"})
-}
-
-func (h *ProxyHandler) listConfigurations(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"configurations": []interface{}{}})
-}
-
-func (h *ProxyHandler) describeConfiguration(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "describeConfiguration not yet implemented"})
-}
-
-func (h *ProxyHandler) listReplicators(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"replicators": []interface{}{}})
-}
-
-func (h *ProxyHandler) createReplicator(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "createReplicator not yet implemented"})
-}
-
-func (h *ProxyHandler) describeReplicator(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "describeReplicator not yet implemented"})
-}
-
-func (h *ProxyHandler) updateReplicationInfo(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "updateReplicationInfo not yet implemented"})
-}
-
-func (h *ProxyHandler) deleteReplicator(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "deleteReplicator not yet implemented"})
-}
-
-func (h *ProxyHandler) listVpcConnections(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"vpcConnections": []interface{}{}})
-}
-
-func (h *ProxyHandler) createVpcConnection(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "createVpcConnection not yet implemented"})
-}
-
-func (h *ProxyHandler) describeVpcConnection(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "describeVpcConnection not yet implemented"})
-}
-
-func (h *ProxyHandler) deleteVpcConnection(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "deleteVpcConnection not yet implemented"})
-}
--- pkg/proxy/internal/adapters/http/sesv2.go
+++ pkg/proxy/internal/adapters/http/sesv2.go
@@ -17,30 +17,12 @@ func (h *ProxyHandler) registerSESRoutes(r chi.Router) {
 
 		r.Post("/email/send", h.sendEmail)
 
-		r.Get("/suppressed-destinations/{email}", h.getSuppressedDestination)
 		r.Get("/suppressed-destinations", h.listSuppressedDestinations)
-		r.Put("/suppressed-destinations/{email}", h.putSuppressedDestination)
-		r.Delete("/suppressed-destinations/{email}", h.deleteSuppressedDestination)
-
-		r.Get("/dedicated-ips", h.getDedicatedIps)
-		r.Get("/dedicated-ips/{ip}", h.getDedicatedIp)
-		r.Post("/dedicated-ip-pools", h.createDedicatedIpPool)
 		r.Get("/dedicated-ip-pools", h.listDedicatedIpPools)
 		r.Get("/dedicated-ip-pools/{poolName}", h.getDedicatedIpPool)
-		r.Delete("/dedicated-ip-pools/{poolName}", h.deleteDedicatedIpPool)
-		r.Put("/dedicated-ip-pools/{poolName}/warmup", h.putDedicatedIpWarmupAttributes)
 		r.Get("/dedicated-ip-pools/{poolName}/warmup", h.getDedicatedIpWarmupAttributes)
-
-		r.Get("/account", h.getAccount)
-		r.Post("/deliverability-test", h.createDeliverabilityTestReport)
-		r.Get("/recommendations", h.listRecommendations)
-
-		r.Post("/custom-verification-email-templates", h.createCustomVerificationEmailTemplate)
 		r.Get("/custom-verification-email-templates", h.listCustomVerificationEmailTemplates)
 		r.Get("/custom-verification-email-templates/{templateName}", h.getCustomVerificationEmailTemplate)
-		r.Put("/custom-verification-email-templates/{templateName}", h.updateCustomVerificationEmailTemplate)
-		r.Delete("/custom-verification-email-templates/{templateName}", h.deleteCustomVerificationEmailTemplate)
-		r.Post("/custom-verification-email-templates/{templateName}/test", h.testRenderEmailTemplate)
 
 		// Email templates (standard)
 		r.Get("/email-templates", h.listEmailTemplates)
@@ -208,36 +190,8 @@ func (h *ProxyHandler) deleteEmailTemplate(w http.ResponseWriter, r *http.Reques
 	writeJSON(w, http.StatusOK, result)
 }
 
-func (h *ProxyHandler) getAccount(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "getAccount not yet implemented"})
-}
-
-func (h *ProxyHandler) getSuppressedDestination(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "getSuppressedDestination not yet implemented"})
-}
-
 func (h *ProxyHandler) listSuppressedDestinations(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]interface{}{"suppressedDestinations": []interface{}{}})
-}
-
-func (h *ProxyHandler) putSuppressedDestination(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "putSuppressedDestination not yet implemented"})
-}
-
-func (h *ProxyHandler) deleteSuppressedDestination(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "deleteSuppressedDestination not yet implemented"})
-}
-
-func (h *ProxyHandler) getDedicatedIps(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"ips": []interface{}{}})
-}
-
-func (h *ProxyHandler) getDedicatedIp(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "getDedicatedIp not yet implemented"})
-}
-
-func (h *ProxyHandler) createDedicatedIpPool(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "createDedicatedIpPool not yet implemented"})
 }
 
 func (h *ProxyHandler) listDedicatedIpPools(w http.ResponseWriter, r *http.Request) {
@@ -248,28 +202,8 @@ func (h *ProxyHandler) getDedicatedIpPool(w http.ResponseWriter, r *http.Request
 	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "getDedicatedIpPool not yet implemented"})
 }
 
-func (h *ProxyHandler) deleteDedicatedIpPool(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "deleteDedicatedIpPool not yet implemented"})
-}
-
-func (h *ProxyHandler) putDedicatedIpWarmupAttributes(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "putDedicatedIpWarmupAttributes not yet implemented"})
-}
-
 func (h *ProxyHandler) getDedicatedIpWarmupAttributes(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "getDedicatedIpWarmupAttributes not yet implemented"})
-}
-
-func (h *ProxyHandler) createDeliverabilityTestReport(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "createDeliverabilityTestReport not yet implemented"})
-}
-
-func (h *ProxyHandler) listRecommendations(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"recommendations": []interface{}{}})
-}
-
-func (h *ProxyHandler) createCustomVerificationEmailTemplate(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "createCustomVerificationEmailTemplate not yet implemented"})
 }
 
 func (h *ProxyHandler) listCustomVerificationEmailTemplates(w http.ResponseWriter, r *http.Request) {
@@ -278,16 +212,4 @@ func (h *ProxyHandler) listCustomVerificationEmailTemplates(w http.ResponseWrite
 
 func (h *ProxyHandler) getCustomVerificationEmailTemplate(w http.ResponseWriter, r *http.Request) {
 	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "getCustomVerificationEmailTemplate not yet implemented"})
-}
-
-func (h *ProxyHandler) updateCustomVerificationEmailTemplate(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "updateCustomVerificationEmailTemplate not yet implemented"})
-}
-
-func (h *ProxyHandler) deleteCustomVerificationEmailTemplate(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "deleteCustomVerificationEmailTemplate not yet implemented"})
-}
-
-func (h *ProxyHandler) testRenderEmailTemplate(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, http.StatusOK, map[string]interface{}{"message": "testRenderEmailTemplate not yet implemented"})
 }
